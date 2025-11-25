c3be7c4d5c94c60573c4e651b96e049286892165
Martin Huter <martin.huter@viesure.io>
2025-11-25 16:16:59 +0100

    Add opencode plugin

--- init.lua
+++ init.lua
@@ -129,18 +129,7 @@ rtp:prepend(lazypath)
 
 require('lazy').setup {
     { import = 'plugins' },
-    {
-        -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-        -- used for completion, annotations and signatures of Neovim apis
-        'folke/lazydev.nvim',
-        ft = 'lua',
-        opts = {
-            library = {
-                -- Load luvit types when the `vim.uv` word is found
-                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-            },
-        },
-    },
+
     { -- Autoformat
         'stevearc/conform.nvim',
         event = { 'BufWritePre' },
