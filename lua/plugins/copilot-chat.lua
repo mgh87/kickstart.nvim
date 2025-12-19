e6131d0c0457ffc8522472c5a21b9a65af758c28
Martin Huter <martin.huter@viesure.io>
2025-12-19 09:39:09 +0100

    Play around with copilot in nvim

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -2,6 +2,7 @@ return {
     {
         'CopilotC-Nvim/CopilotChat.nvim',
         dependencies = {
+            { 'github/copilot.vim' },
             { 'nvim-lua/plenary.nvim', branch = 'master' },
         },
         build = 'make tiktoken',
